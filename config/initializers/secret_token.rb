fb9cefcf5cc58879449ef43766c80ecb4ab0fb41
root <root@ste-projekt.local.site>
2013-11-28 13:45:41 +0100

    Moj testni commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+B2book::Application.config.secret_token = 'd3f40c7e3af570ce7afd6fb5632702f2d87491c6c585ac65d37a9d40985e760fe16b59b58fc1d4ac77f4ec8515c656e748dc194b0af16ca985445a180914a2b6'
